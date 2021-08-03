accce9aeb17ed0a894f06fa602fa2d0d81268b35
201710757 <neti69@naver.com>
2021-08-03 22:49:15 +0900

    q value update v1

--- QTABLE/qLearning.py
+++ QTABLE/qLearning.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 
 GAMMA = 0.99
+LR = 0.1
+
 env = gym.make('FrozenLake-v0')
 ACTION_SPACE = env.action_space.n
 OBSERVATION_SPACE = env.observation_space.n
@@ -14,18 +16,21 @@ def pick_action(state):
     else:
         return np.argmax(state)
 
-success = 0
-episodes = 1000
+r_list = []
+episodes = 10000
 for ep in range(episodes):
     obs = env.reset()
-
+    success = 0
     while True:
+        # env.render()
         a = pick_action(Q_table[obs,:])
         n_obs, reward, done, info = env.step(a)
 
-        Q_table[obs, a] = reward + np.max(Q_table[n_obs, :])
+        Q_table[obs, a] = (1-LR)*(Q_table[obs, a]) +  LR*(reward + GAMMA * np.max(Q_table[n_obs, :]))
         obs = n_obs
         success += reward
         if done:
             break
-print("Success Rate : {}".format(success))
+    # env.render()
+    r_list.append(success)
+print(r_list)
