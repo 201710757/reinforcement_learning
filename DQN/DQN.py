37043c355cc4f2c321c517dd051f801e8663fc61
201710757 <neti69@naver.com>
2021-08-05 16:00:01 +0900

    DQN 2013 (not yet : buffer pop)

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -42,3 +42,17 @@ class DQN(nn.Module):
         state = torch.tensor(state).float()
         # state = torch.tensor(state, dtype=torch.float32)
         return self.forward(state).argmax()
+
+class qnet(nn.Module):
+    def __init__(self, inputs, outputs):
+        super(qnet, self).__init__()
+        self.layer1 = nn.Linear(inputs, 64)
+        self.layer2 = nn.Linear(64, 128)
+        self.layer3 = nn.Linear(128, 32)
+        self.head = nn.Linear(32, outputs)
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        return self.head(x)
