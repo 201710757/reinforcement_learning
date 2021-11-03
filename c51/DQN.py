195c0c4487b2b85eb001c45300c02d88d2048360
201710757 <neti69@naver.com>
2021-11-03 17:33:43 +0900

    c51 - before projection

--- c51/DQN.py
+++ c51/DQN.py
@@ -8,7 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class qnet(nn.Module):
-    def __init__(self, inputs, outputs):
+    def __init__(self, outputs):
         super(qnet, self).__init__()
         self.feature_extraction = nn.Sequential(
             nn.Conv2d(4, 32, kernel_size=8, stride=4),
@@ -20,12 +20,13 @@ class qnet(nn.Module):
         )
         self.fc = nn.Linear(7 * 7 * 64, 512)
         
-        self.fc_q = nn.Linear(512, 2 * 51)
+        self.fc_q = nn.Linear(512, outputs * 51)
 
     def forward(self, x):
+        mb_size = x.size(0)
         x = self.feature_extraction(x / 255.0)
         x = F.relu(self.fc(x))
         
-        action_value = F.softmax(self.fc_q(x))
+        action_value = F.softmax(self.fc_q(x).view(mb_size, outputs, 51), dim=2)
 
         return action_value
