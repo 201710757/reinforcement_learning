d39fd3d5295d2de09d429d0db122f14111672266
201710757 <neti69@naver.com>
2022-02-25 14:54:00 +0900

    error test

--- PG/vanilla_ActorCritic.py
+++ PG/vanilla_ActorCritic.py
@@ -42,8 +42,8 @@ for ep in range(MAX_EP):
         action = policy(s)
         s, r, d, _ = env.step(action)
         
-        gpu_reward = torch.tensor(r).to(device)
-        policy.rewards.append(gpu_reward)
+        # gpu_reward = torch.tensor(r).to(device)
+        policy.rewards.append(r)
         ep_reward += r
 
 
