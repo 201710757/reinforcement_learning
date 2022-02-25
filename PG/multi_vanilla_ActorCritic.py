124e23f781667ce329e4fbdf3f72961ca6bb8981
201710757 <neti69@naver.com>
2022-02-25 15:07:19 +0900

    Actor Critic MultiProcessing

---
+++ PG/multi_vanilla_ActorCritic.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+
+import MultiPro
+import matplotlib.pyplot as plt
+import numpy as np
+import gym
+from ActorCritic import ActorCritic
+
+device = torch.device("cuda")
+worker = 4
+env_name = 'CartPole-v1'
+env = MultiPro.SubprocVecEnv([lambda: gym.make(env_name) for i in range(worker)])
+
+input_dim = env.observation_space.shape[0]
+hidden_dim = 1024
+output_dim = env.action_space.n
+
+policy = ActorCritic(input_dim, hidden_dim, output_dim).to(device)
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_normal_(m.weight)
+        m.bias.data.fill_(0)
+policy.apply(init_weights)
+
+LR = 1e-3
+optimizer = optim.Adam(policy.parameters(), lr = LR)
+policy.train()
+
+MAX_EP = 500
+# GAMMA = 0.99
+
+train_reward = []
+for ep in range(MAX_EP):
+    ep_reward = 0
+
+    s = env.reset()
+    d = np.array([False])
+    while not d.any():
+        s = torch.FloatTensor(s).to(device).unsqueeze(0)
+        action = policy(s)
+        s, r, d, _ = env.step(action.reshape(-1).cpu().data.numpy())
+        
+        gpu_reward = torch.tensor(r).to(device)
+        policy.rewards.append(gpu_reward)
+        ep_reward += r
+
+
+    optimizer.zero_grad()
+    loss = policy.loss()
+    loss.backward()
+    optimizer.step()
+
+    policy.clearMemory()
+
+    train_reward.append(ep_reward)
+    
+    if ep % 10 == 0:
+        print("EP : {} | Mean Reward : {}".format(ep, np.mean(train_reward[-10:])))
+    if np.mean(train_reward[-10:]) >= 475:
+        print("CLEAR!!")
+        break
+
+
+
+
+
+
