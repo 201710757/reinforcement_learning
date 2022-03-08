049617a6c831936b2329679a41802dfd2351c567
201710757 <neti69@naver.com>
2022-03-09 02:04:19 +0900

    meta rl folder clear

---
+++ metaRL/baseline_A3C/ActorCritic.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+
+device = torch.device("cuda")
+
+class ActorCritic(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout = 0.5):
+        super(ActorCritic, self).__init__()
+
+        self.affine = nn.Linear(input_dim, hidden_dim)
+
+        # Actor
+        self.actor_aff = nn.Linear(hidden_dim, hidden_dim)
+        self.action_layer = nn.Linear(hidden_dim, output_dim)
+
+        # Critic
+        self.critic_aff = nn.Linear(hidden_dim ,hidden_dim)
+        self.value_layer = nn.Linear(hidden_dim, 1)
+
+        # use it now!
+        self.dropout = nn.Dropout(dropout)
+        
+        self.log_prob_actions = []
+        self.state_values = []
+        self.rewards = []
+
+    def forward(self, state):
+        state = self.affine(state)
+        state = F.relu(state)
+        #state = self.dropout(state)
+
+        #state_v = self.critic_aff(state)
+        #state_v = F.relu(state_v)
+        #state_v = self.dropout(state_v)
+        state_value = self.value_layer(state)
+        # state_value = self.dropout(state_value)
+        
+
+        #action_v = self.actor_aff(state)
+        #action_v = F.relu(action_v)
+        #action_v = self.dropout(action_v)
+        #action_prob = F.softmax(self.action_layer(action_v), dim=-1)
+        action_value = self.action_layer(state)
+        #dist = Categorical(action_prob)
+        #action = dist.sample()
+
+        #self.log_prob_actions.append(dist.log_prob(action))
+        #self.state_values.append(state_value)
+
+        return state_value, action_value
+
+    def loss(self, GAMMA=0.99, normalize = True):
+        returns = []
+        R = 0
+        for r in reversed(self.rewards):
+            R = r + GAMMA * R
+            returns.insert(0, R)
+        # print(returns)
+        returns = torch.tensor(returns).to(device)
+        if normalize:
+            returns = (returns - returns.mean()) / returns.std()
+
+        # will be reduced - code
+        loss = 0
+        for logprob, value, reward in zip(self.log_prob_actions, self.state_values, returns):
+            advantage = reward - value.reshape(-1)
+            action_loss = -logprob * advantage
+            value_loss = F.smooth_l1_loss(value, reward)
+
+            loss += (action_loss.sum().reshape(-1).float() + value_loss.float())
+            #print(loss)
+        return loss
+
+    # Why?
+    def clearMemory(self):
+        del self.log_prob_actions[:]
+        del self.state_values[:]
+        del self.rewards[:]
---
+++ metaRL/baseline_A3C/simple_A3C.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import gym
+from ActorCritic import ActorCritic
+import torch.multiprocessing as mp
+
+device = torch.device("cuda:0")
+# env_name = 'CartPole-v1'
+env_name = 'LunarLander-v2'
+env = gym.make(env_name)
+
+writer = SummaryWriter("runs/"+ env_name)
+
+input_dim = env.observation_space.shape[0]
+hidden_dim = 1024
+output_dim = env.action_space.n
+LR = 1e-3
+MAX_EP = 5000
+GAMMA = 0.99
+
+# 4 : memory error
+process_num = 3
+
+
+def train(g_policy, model_num):
+    env = gym.make(env_name)
+    local_policy = ActorCritic(input_dim, hidden_dim, output_dim).to(device)
+    local_policy.load_state_dict(g_policy.state_dict())
+    
+    local_optimizer = optim.Adam(g_policy.parameters(), lr = LR)
+
+
+    train_reward = []
+    for ep in range(MAX_EP):
+        ep_reward = 0
+
+        log_prob_actions = []
+        state_values = []
+        rewards = []
+
+        d = False
+        
+        s = env.reset()
+        while not d:
+            s = torch.FloatTensor(s).to(device).unsqueeze(0)
+            state_pred, action_pred = local_policy(s)
+            action_prob = F.softmax(action_pred, dim=-1)
+            dist = Categorical(action_prob)
+            action = dist.sample()
+            #log_prob_action = dist.log_prob(action)
+
+            s, r, d, _ = env.step(action.item())
+            
+            log_prob_actions.append(dist.log_prob(action))
+            state_values.append(state_pred)
+            rewards.append(r)
+
+            ep_reward += r
+        log_prob_actions = torch.cat(log_prob_actions).to(device)
+        state_values = torch.cat(state_values).squeeze(-1).to(device)
+
+        returns = []
+        R = 0
+        for r in reversed(rewards):
+            R = r + GAMMA*R
+            returns.insert(0, R)
+        returns = torch.tensor(returns).float().to(device)
+        returns = (returns - returns.mean()) / returns.std()
+        
+        advantage = returns - state_values
+        advantage = (advantage - advantage.mean()) / advantage.std()
+
+        advantage = advantage.detach()
+        returns = returns.detach()
+        action_loss = -(advantage * log_prob_actions).sum()
+        value_loss = F.smooth_l1_loss(state_values, returns).sum()
+        
+        loss = action_loss + value_loss
+
+        local_optimizer.zero_grad()
+
+        loss.backward()
+        #action_loss.backward(retain_graph=True)
+        #value_loss.backward()
+
+        for g_param, l_param in zip(g_policy.parameters(), local_policy.parameters()):
+            g_param._grad = l_param._grad
+        local_optimizer.step()
+
+        local_policy.load_state_dict(g_policy.state_dict())
+
+        train_reward.append(ep_reward)
+        
+        if ep % 10 == 0 and model_num == 0:
+            writer.add_scalar("Model - Average 10 steps", np.mean(train_reward[-100:]), ep)
+
+        if ep % 100 == 0:
+            print("MODEL{} - EP : {} | Mean Reward : {}".format(model_num, ep, np.mean(train_reward[-100:])))
+
+def init_weights(m):
+        if type(m) == nn.Linear:
+            torch.nn.init.xavier_normal_(m.weight)
+            m.bias.data.fill_(0)
+
+
+if __name__ == "__main__":
+    
+
+    global_policy = ActorCritic(input_dim, hidden_dim, output_dim).to(device)
+    global_policy.share_memory()
+
+    processes = []
+
+    global_policy.apply(init_weights)
+    global_policy.train()
+
+
+    try:
+        mp.set_start_method('spawn')
+        print("MP start method:",mp.get_start_method())
+    except:
+        pass
+
+    for rank in range(process_num):
+        p = mp.Process(target=train, args=(global_policy,rank))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
