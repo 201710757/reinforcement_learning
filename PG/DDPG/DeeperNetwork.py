d7b17d8b67c035d6ad1d5dcd0b92332802c54ee5
201710757 <neti69@naver.com>
2022-03-23 01:05:26 +0900

    mujoco DDPG

---
+++ PG/DDPG/DeeperNetwork.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda:0")
+
+class Mu(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Mu, self).__init__()
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.fc1 = nn.Linear(self.input_dim, 1024)
+        # = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        # = nn.BatchNorm1d(1024)
+        self.fc3 = nn.Linear(1024, 64)
+        # = nn.BatchNorm1d(64)
+        self.mu = nn.Linear(64, self.output_dim)
+
+    def forward(self, x):
+        #x = x.unsqueeze(0)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        m = torch.tanh(self.mu(x))
+        
+        return m
+
+
+
+
+class Q(nn.Module):
+    def __init__(self, input_dim):
+        super(Q, self).__init__()
+
+        self.input_dim = input_dim
+
+        
+        self.fc_s = nn.Linear(self.input_dim, 1024)
+        # = nn.BatchNorm1d(512)
+        
+        self.fc_a = nn.Linear(1, 256)
+        # = nn.BatchNorm1d(256)
+        self.fc_a1 = nn.Linear(256,1024)
+        # = nn.BatchNorm1d(512)
+
+        self.fc_Q = nn.Linear(2048, 2048)
+         #= nn.BatchNorm1d(1024)
+        self.fc_Q1 = nn.Linear(2048, 128)
+         #= nn.BatchNorm1d(64)
+
+        self.out = nn.Linear(128, 1)
+
+    def forward(self, s, a):
+        #s = s.unsqueeze(0)
+        #a = a.unsqueeze(0)
+
+        _s = F.relu(self.fc_s(s))
+        _a = F.relu(self.fc_a(a))
+        _a = F.relu(self.fc_a1(_a))
+
+        sa = torch.cat([_s, _a], dim=1)
+
+        Q = F.relu(self.fc_Q(sa))
+        Q = F.relu(self.fc_Q1(Q))
+        Q = self.out(Q)
+
+        return Q
---
+++ PG/DDPG/mujocoMain.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+import gym
+
+from DeeperNetwork import Q, Mu
+from OrnsteinUhlenbeckNoise import OrnsteinUhlenbeckNoise
+from ReplayBuffer import ReplayBuffer
+
+from torch.utils.tensorboard import SummaryWriter
+import time
+
+device = torch.device("cuda:0")
+
+
+LR_Q = 0.001
+LR_Mu = 0.0005
+GAMMA = 0.99
+tau = 0.005
+MAX_STEPS = 10000000
+batch_size = 128
+UPDATE_SIZE = 2000
+SOFT_UPDATE_TERM = 10
+
+def train(mu, mu_target, q, q_target, memory, q_optim, mu_optim):
+    s, a, r, sp, d = memory.sample(batch_size)
+    target = r + GAMMA * q_target(sp, torch.argmax(mu_target(sp), dim=1, keepdim=True).float()) * d
+    #target = r + GAMMA * q_target(sp,mu_target(sp)) * d
+    critic_loss = F.mse_loss(target.detach(), q(s, a))
+    q_optim.zero_grad()
+    critic_loss.backward()
+    q_optim.step()
+
+    actor_loss = -q(s, torch.argmax(mu(s), dim=1, keepdim=True).float()).mean()
+    #actor_loss = -q(s, mu(s)).mean()
+    mu_optim.zero_grad()
+    actor_loss.backward()
+    mu_optim.step()
+
+def soft_update(net, net_grad):
+    for p_t, p in zip(net_grad.parameters(), net.parameters()):
+        p_t.data.copy_(tau * p.data + (1.0 - tau) * p_t.data)
+
+def main():
+    env_name = 'Humanoid-v2'
+    writer = SummaryWriter("runs/"+ env_name + "_" + time.ctime(time.time()))
+
+    env = gym.make(env_name)
+
+    input_dim = env.observation_space.shape[0]
+    output_dim = env.action_space.shape[0]#env.action_space.n
+    memory = ReplayBuffer()
+
+    q, q_target = Q(input_dim).to(device), Q(input_dim).to(device)
+    mu, mu_target = Mu(input_dim, output_dim).to(device), Mu(input_dim, output_dim).to(device)
+
+    q_target.load_state_dict(q.state_dict())
+    mu_target.load_state_dict(mu.state_dict())
+
+    q_optim = optim.Adam(q.parameters(), lr = LR_Q)
+    mu_optim = optim.Adam(mu.parameters(), lr = LR_Mu)
+    N = OrnsteinUhlenbeckNoise(mu=np.zeros(1))
+
+    total_score = 0
+    for ep in range(MAX_STEPS):
+        s = env.reset()
+        d = False
+        r = 0
+        while not d:
+            a = mu(torch.FloatTensor(s).to(device))
+            noise = N()[0]
+            a = torch.argmax(a * noise).item()#N()[0]
+            #print(a)
+            #a = a.item() + noise
+            #print(a)
+            sp, r, d, _ = env.step([a])
+            memory.put((s, a, r, sp, d))
+            
+            total_score += r
+            
+            s = sp
+
+        if memory.size() > UPDATE_SIZE:
+            for _ in range(SOFT_UPDATE_TERM):
+                train(mu, mu_target, q, q_target, memory, q_optim, mu_optim)
+                soft_update(mu, mu_target)
+                soft_update(q, q_target)
+
+        if ep % 10 == 0 and ep != 0:
+            writer.add_scalar("Model - Average 10 steps", total_score/10, ep)
+            print("{} EP | REWARD : {}".format(ep, total_score/10))
+            total_score = 0
+
+main()
+
